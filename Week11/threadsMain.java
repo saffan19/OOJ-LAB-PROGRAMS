b625957eb08948ad0c8173559be36746a294af7d
saffan19 <67745780+saffan19@users.noreply.github.com>
2020-12-08 10:26:22 +0530

    Add files via upload

---
+++ Week11/threadsMain.java
@@ -0,0 +1,32 @@
+class printString extends Thread{
+    String s;
+    int time;
+    printString(String st,int t)
+    {
+        this.s=st;
+        this.time=t;
+    }
+    public void run(){
+        while(true)
+        {
+            try{
+                Thread.sleep(time);
+                System.out.println(s); 
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+public class threadsMain
+{
+    public static void main(String args[])
+    {
+        printString pb=new printString("BMS College of Engineering",10000);
+        printString pc=new printString("CSE",2000);
+        pb.start();
+        pc.start();
+
+    }
+}
